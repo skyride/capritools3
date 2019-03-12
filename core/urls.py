a0007beaa300c1de88bf33a74e518c5b86e0ce83
Adam Findlay <skylinerspeeder@gmail.com>
2019-03-12 10:41:09 +0000

    Switched homepage to a TemplateView in urls

--- core/urls.py
+++ core/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
+from django.views.generic.base import TemplateView
 
 from . import views
 
 app_name = "dscan"
 urlpatterns = [
-    path("", views.home)
+    path("", TemplateView.as_view(template_name="core/home.html"), name="home")
 ]
