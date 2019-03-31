a730ed0f449bc19adc062ce85817f821813dd7b5
Adam Findlay <adam.findlay@mercurytide.co.uk>
2019-03-31 00:29:21 +0000

    Added production configuration

--- capritools/settings.py
+++ capritools/settings.py
@@ -140,4 +140,5 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/dev/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = "/static/"
+STATIC_ROOT = "/static/"
